ab6a6ab4b02550d2e89497e62ddcf95861b848ab
Justin <justinv403@gmail.com>
2022-05-01 11:59:15 -0500

    More level editor prep
    Added some more changes to things to get stuff to work

--- support.py
+++ support.py
@@ -1,14 +1,9 @@
-from os import walk
-import pygame
+from csv import reader
 
-
-def import_folder(path):
-    surface_list = []
-
-    for _,__,image_files in walk(path): # the underscores are unneeded information
-        for image in image_files:
-            full_path = path + "/" + image
-            image_surface = pygame.image.load(full_path).convert_alpha()
-            surface_list.append(image_surface)
-
-    return surface_list
+def import_csv_layout(path):
+    terrain_map = []
+    with open(path) as map:
+        level = reader(map, delimiter=",")
+        for row in level:
+            terrain_map.append(list(row))
+        return terrain_map
